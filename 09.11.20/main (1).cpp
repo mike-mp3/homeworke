3c4c5b5a2e996277cc4f25c65c038da2ef320ff5
mike-mp3 <73157813+mike-mp3@users.noreply.github.com>
2020-11-10 10:31:46 +0300

    Delete main (1).cpp

--- 09.11.20/main (1).cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Черный черствый хлеб,"<<endl<<"Тупой ломает нож."<<endl<<"Уставлены глаза"<<endl<<"В размытый горизонт"<<endl<<".";
-}
-//номер 4
